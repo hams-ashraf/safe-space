9599180d406c6f46484fb1b12572971b71f0495f
yasseryarah980-jpg <Yasseryarah980@gmail.com>
2026-03-10 21:52:01 +0200

    Add all change

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,8 +1,7 @@
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api", 
+  baseURL: "http://safespace.runasp.net/api",
   headers: { "Content-Type": "application/json" },
 });
 
